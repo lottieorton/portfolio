7951d7ea8dc17d0e312f2b98120fb781c84b734e
LottieOrton <lottieorton11@gmail.com>
2026-09-22 12:43:33 +1000

    updates nav and header sections with other small refactors

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,23 +1,24 @@
 const headingText = "Lottie Orton";
-const subHeadingText = "Junior Software Engineer";
+const subHeadingText = "Junior Software Engineer ";
 let i = 0;
 let j = 0;
 export function typingHeader() {
   const headerElement = document.querySelector("h1");
   const subHeaderElement = document.querySelector("#sub-heading");
-  subHeaderElement.classList.add("typewriter");
 
   if (i < headingText.length) {
     headerElement.textContent += headingText.charAt(i);
     i++;
     setTimeout(typingHeader, 100);
-  } else if (j < subHeadingText.length) {
+  } else if (i === headingText.length && j === 0) {
     headerElement.classList.remove("typewriter");
+    subHeaderElement.classList.add("typewriter");
     subHeaderElement.textContent += subHeadingText.charAt(j);
     j++;
     setTimeout(typingHeader, 100);
-  } else if (j === subHeadingText.length) {
-    subHeaderElement.classList.remove("typewriter");
-    headerElement.classList.add("typewriter");
+  } else if (j < subHeadingText.length) {
+    subHeaderElement.textContent += subHeadingText.charAt(j);
+    j++;
+    setTimeout(typingHeader, 100);
   }
 }
